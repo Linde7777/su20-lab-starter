9bde3e48d3fe756e116367d129804adcd843629d
root <longpeng96@gmail.com>
2022-04-14 10:54:30 +0800

    factorial.s pass

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,29 +22,14 @@ main:
 
 
 factorial:
-    
-    beq a0,x0,label1
-label1:
-    addi a0,x0,0
-    jr ra
-
-    beq a0,1,label2
-label2:
-    addi a0,x0,1
-    jr ra
-
-    addi t1,x0,0    #num1
-    addi t2,x0,1    #num2
-    addi t3,x0,0    #total
-    addi t4,x0,2    #i
+    addi s0,x0,1    #result=1   
+    add s1,x0,a0   #i=n
 loop:
-    addi t5,a0,1        #t5=a0+1
-    bge t4,t5,label3    #if(t4>=a0+1) goto label3
-    addi t3,t1,t2       #total=num1+num2
-    mv t1,t2            #num1=num2
-    mv t2,t3            #num2=total
+    beq s1,x0,label1    #if(i==0) goto label1
+    mul s0,s0,s1     #result*=i
+    addi s1,s1,-1   #i-=1
     jal x0,loop
 
-label3:
-    mv a0,t3            #a0=total
+label1:
+    mv a0,s0    #a0=result
     jr ra
