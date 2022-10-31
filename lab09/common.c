7e802208fa320b20995a37baff16379d6a20b40a
root <longpeng96@gmail.com>
2022-10-31 09:10:52 +0800

    ex2 v1

--- lab09/common.c
+++ lab09/common.c
@@ -1,79 +1,104 @@
-#include <time.h>
-#include <stdio.h>
-#include <x86intrin.h>
 #include "common.h"
+#include <emmintrin.h>
+#include <stdio.h>
+#include <time.h>
+#include <x86intrin.h>
 
 long long int sum(unsigned int vals[NUM_ELEMS]) {
-	clock_t start = clock();
+  clock_t start = clock();
 
-	long long int sum = 0;
-	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		for(unsigned int i = 0; i < NUM_ELEMS; i++) {
-			if(vals[i] >= 128) {
-				sum += vals[i];
-			}
-		}
-	}
-	clock_t end = clock();
-	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
-	return sum;
+  long long int sum = 0;
+  for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+    for (unsigned int i = 0; i < NUM_ELEMS; i++) {
+      if (vals[i] >= 128) {
+        sum += vals[i];
+      }
+    }
+  }
+  clock_t end = clock();
+  printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+  return sum;
 }
 
 long long int sum_unrolled(unsigned int vals[NUM_ELEMS]) {
-	clock_t start = clock();
-	long long int sum = 0;
+  clock_t start = clock();
+  long long int sum = 0;
 
-	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
-			if(vals[i] >= 128) sum += vals[i];
-			if(vals[i + 1] >= 128) sum += vals[i + 1];
-			if(vals[i + 2] >= 128) sum += vals[i + 2];
-			if(vals[i + 3] >= 128) sum += vals[i + 3];
-		}
+  for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+    for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+      if (vals[i] >= 128)
+        sum += vals[i];
+      if (vals[i + 1] >= 128)
+        sum += vals[i + 1];
+      if (vals[i + 2] >= 128)
+        sum += vals[i + 2];
+      if (vals[i + 3] >= 128)
+        sum += vals[i + 3];
+    }
 
-		//This is what we call the TAIL CASE
-		//For when NUM_ELEMS isn't a multiple of 4
-		//NONTRIVIAL FACT: NUM_ELEMS / 4 * 4 is the largest multiple of 4 less than NUM_ELEMS
-		for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
-			if (vals[i] >= 128) {
-				sum += vals[i];
-			}
-		}
-	}
-	clock_t end = clock();
-	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
-	return sum;
+    // This is what we call the TAIL CASE
+    // For when NUM_ELEMS isn't a multiple of 4
+    // NONTRIVIAL FACT: NUM_ELEMS / 4 * 4 is the largest multiple of 4 less than
+    // NUM_ELEMS
+    for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+      if (vals[i] >= 128) {
+        sum += vals[i];
+      }
+    }
+  }
+  clock_t end = clock();
+  printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+  return sum;
 }
 
 long long int sum_simd(unsigned int vals[NUM_ELEMS]) {
-	clock_t start = clock();
-	__m128i _127 = _mm_set1_epi32(127);		// This is a vector with 127s in it... Why might you need this?
-	long long int result = 0;				   // This is where you should put your final result!
-	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
-	
-	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* YOUR CODE GOES HERE */
+  clock_t start = clock();
+  __m128i _127 = _mm_set1_epi32(
+      127); // This is a vector with 127s in it... Why might you need this?
+  long long int result = 0; // This is where you should put your final result!
+  /* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
 
-		/* You'll need a tail case. */
+  for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+    /* YOUR CODE GOES HERE */
+    __m128i formerValues = _mm_setzero_si128();
+    __m128i currentValues;
+    unsigned int i;
+    for (i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+      currentValues = _mm_loadu_si128((__m128i *)vals + i);
+      __m128i cmp = _mm_cmpgt_epi32(currentValues, _127);
+      // if vectorValues > _127, its values wouldn't be changed
+      currentValues = _mm_and_si128(currentValues, cmp);
+      currentValues = _mm_add_epi32(currentValues, formerValues);
+      formerValues = currentValues;
+    }
 
-	}
-	clock_t end = clock();
-	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
-	return result;
+    /* You'll need a tail case. */
+    int *tempResult = (int *)malloc(sizeof(int) * 4);
+    _mm_storeu_si128((__m128i *)tempResult, formerValues);
+    for (; i < NUM_ELEMS; i++) {
+      *tempResult += *(vals + i);
+    }
+
+    for (int i = 0; i < 4; i++) {
+      result += *(tempResult + i);
+    }
+  }
+  clock_t end = clock();
+  printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+  return result;
 }
 
 long long int sum_simd_unrolled(unsigned int vals[NUM_ELEMS]) {
-	clock_t start = clock();
-	__m128i _127 = _mm_set1_epi32(127);
-	long long int result = 0;
-	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* COPY AND PASTE YOUR sum_simd() HERE */
-		/* MODIFY IT BY UNROLLING IT */
+  clock_t start = clock();
+  __m128i _127 = _mm_set1_epi32(127);
+  long long int result = 0;
+  for (unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+    /* COPY AND PASTE YOUR sum_simd() HERE */
+    /* MODIFY IT BY UNROLLING IT */
 
-		/* You'll need 1 or maybe 2 tail cases here. */
-
-	}
-	clock_t end = clock();
-	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
-	return result;
+    /* You'll need 1 or maybe 2 tail cases here. */
+  }
+  clock_t end = clock();
+  printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+  return result;
 }
