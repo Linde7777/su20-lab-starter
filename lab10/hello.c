4786181a23c3f2228c3c1977f0bb1a7169d2c10b
root <longpeng96@gmail.com>
2022-11-01 13:56:29 +0800

    v1

--- lab10/hello.c
+++ lab10/hello.c
@@ -2,9 +2,11 @@
 #include <omp.h>
 
 int main() {
+    omp_set_num_threads(3);
 	#pragma omp parallel
 	{
 		int thread_ID = omp_get_thread_num();
 		printf(" hello world %d\n", thread_ID);
 	}
+
 }
